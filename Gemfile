7731cf760e90b1683535c8d5d92b0c7a2d1ad138
youcune <you@nakanishi.email>
2017-08-23 11:54:16 +0900

    add middleman-deploy

--- Gemfile
+++ Gemfile
@@ -3,7 +3,8 @@ source 'https://rubygems.org'
 gem 'middleman'
 gem 'middleman-autoprefixer'
 
-gem 'middleman-deploy'
+# MiddleMan 4.2系で動かなくなっているので、PRの出ている対応版を明示的に指定
+gem 'middleman-deploy', github: 'shawnzvinis/middleman-deploy'
 
 gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
 gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -40,7 +40,12 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.branch = 'master'
+end
